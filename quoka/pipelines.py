beedeea4992931c41a8f2ce7a5104b5fa4c92461
Philip Brinkmann <webmaster@phi-fotos.de>
2016-09-10 21:18:24 +0200

    changed date processing

--- quoka/pipelines.py
+++ quoka/pipelines.py
@@ -50,8 +50,6 @@ class QuokaPipeline(object):
 			item['Kaufpreis'] = eval(Kaufpreis)
 		except:
 			item['Kaufpreis'] = 0 # default value if no value is given
-		item['Monat'] = date.today().month # !!!was soll hier wirklich hin???
-		#item['url'] = item['url'][0] # the url is in the string that is on position 0 in the array
 		item['Telefon'] = 0 # !!!muss noch geschrieben werden!!!
 		for d in item['Erstellungsdatum']: # extract() gibt zuviele Zeilen zurueck, aber nur die Datumszeile enthaelt nicht-Whitespaces
 			d = d.strip()
@@ -61,9 +59,16 @@ class QuokaPipeline(object):
 				elif "Gestern" in d:
 					yesterday = date.today() - timedelta(1)
 					item['Erstellungsdatum'] = eval(yesterday.strftime("%Y%m%d")) # yesterday; format: yyyymmdd as integer
+				elif "vor" in d:
+					# it can happen that the date reads "vor 6 Monaten" or similar
+					if "Monat" in d:
+						day = date.today() - timedelta(eval(d.split(' ')[1])*30) # count every month with 30 days to get an as accurate date as possible
+					elif "Jahr" in d:
+						day = date.today() - timedelta(eval(d.split(' ')[1])*365) # count every year with 365 days to get an as accurate date as possible
+					item['Erstellungsdatum'] = eval(day.strftime("%Y%m%d")) # day; format: yyyymmdd as integer
 				else:
 					d = d.split(".")	# now d is an array of strings: [day, month, year]
-					item['Erstellungsdatum'] =  eval(d[2]+d[1]+d[0]) # kann auch "Heute, um..." oder "Gestern" sein!!!
+					item['Erstellungsdatum'] =  eval(d[2]+d[1]+d[0])
 				break
 		# NOW: insert the data into the database if it is not already there
 		self.cursor.execute("SELECT * FROM quokascrapedata WHERE OBID=?", (item['OBID'],)) # get the entry (if it exists)
